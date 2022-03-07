4f30f6cadf2f9990eabd92d58d256cee42e54985
jxingyang139 <jxingyang@126.com>
2022-03-07 15:39:55 +0800

    [tuya][wifi] update none reference file

--- platform/os/Huawei_LiteOS/module_config.mk
+++
@@ -1,2 +0,0 @@
-res_cfg_srcs := kernel/redirect
-CCFLAGS += -I$(MAIN_TOPDIR)/platform/os/Huawei_LiteOS/kernel/extended/include
--- third_party/mbedtls/module_config.mk
+++
@@ -1,5 +0,0 @@
-mbedtls_srcs := library
-CCFLAGS += -I$(MAIN_TOPDIR)/third_party/mbedtls/include \
-	-I$(MAIN_TOPDIR)/third_party/mbedtls/include/mbedtls \
-	-I$(MAIN_TOPDIR)/platform/drivers/cipher \
-	-I$(MAIN_TOPDIR)/components/lwip_sack/include
